0f0bc4a5ca19dd125488d17d72c257d1d46c2d47
viralgupta <guptaviral0@gmail.com>
2023-12-28 02:00:15 +0530

    all commit reverted

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import LeftCanvas from "./components/LeftCanvas";
-import LeaderBoard from "./components/RightCanvas";
+import RightCanvas from "./components/RightCanvas";
 
 export default function Home() {
   const [number_of_bars, setNumber_of_bars] = useState(50);
@@ -14,8 +14,9 @@ export default function Home() {
       active: false,
     },
   ]);
-  const [Sorting, setSorting] = useState(false);
-  const [timetosort, setTimetosort] = useState(0);
+  const [Sorting, setSorting] = useState(false)
+  const [timetosort, setTimetosort] = useState(0)
+
 
   useEffect(() => {
     let newArray = Array.from({ length: number_of_bars }, (_, index) => ({
@@ -29,6 +30,7 @@ export default function Home() {
     setSampleArray((s) => (s.length > 1 ? randomizeArray([...s]) : s));
   };
 
+  
   function randomizeArray(
     array: { height: number; active: boolean }[]
   ): { height: number; active: boolean }[] {
@@ -39,10 +41,11 @@ export default function Home() {
     return array;
   }
 
+
   function bubbleSortByHeight() {
     let arr = [...sampleArray];
-    const date1 = Date.now();
-    setTimetosort(0);
+    const date1 = Date.now()
+    setTimetosort(0)
 
     const n = arr.length;
     let i = 0;
@@ -51,7 +54,7 @@ export default function Home() {
     const sortStep = () => {
       arr.forEach((bar) => (bar.active = false));
       if (i < n - 1) {
-        setSorting((s) => (s == false ? true : true));
+        setSorting(s => s == false ? true : true)
         if (j < n - i - 1) {
           arr[j].active = true;
           arr[j + 1].active = true;
@@ -71,17 +74,18 @@ export default function Home() {
           j = 0;
           setTimeout(sortStep, time);
         }
-      } else {
-        setTimetosort(Date.now() - date1);
-        setSorting((s) => (s == true ? false : false));
+      }
+      else{
+        setTimetosort(Date.now() - date1)
+        setSorting(s => s == true ? false : false)
       }
     };
     sortStep();
   }
 
   function selectionSortByHeight() {
-    const date1 = Date.now();
-    setTimetosort(0);
+    const date1 = Date.now()
+    setTimetosort(0)
     let arr = [...sampleArray];
 
     const n = arr.length;
@@ -93,7 +97,7 @@ export default function Home() {
 
       if (i < n - 1) {
         let minIndex = i;
-        setSorting((s) => (s == false ? true : true));
+        setSorting(s => s == false ? true : true)
 
         if (j < n) {
           arr[i].active = true;
@@ -104,81 +108,29 @@ export default function Home() {
             arr[j] = arr[minIndex];
             arr[minIndex] = temp;
           }
-
+          
           setSampleArray([...arr]);
           j++;
           setTimeout(sortStep, time);
-        } else {
+        } else {          
           i++;
           j = i + 1;
           setTimeout(sortStep, time);
         }
-      } else {
-        setTimetosort(Date.now() - date1);
-        setSorting((s) => (s == true ? false : false));
+      }
+      else{
+        setTimetosort(Date.now() - date1)
+        setSorting(s => s == true ? false : false)
       }
     };
 
     sortStep();
   }
-
-  const usersData = [
-    { username: "user1", profilePicture: "url1", points: 30 },
-    { username: "user2", profilePicture: "url2", points: 80 },
-    { username: "user3", profilePicture: "url3", points: 45 },
-    { username: "user4", profilePicture: "url4", points: 60 },
-    { username: "user5", profilePicture: "url5", points: 90 },
-    { username: "user6", profilePicture: "url6", points: 110 },
-    { username: "user7", profilePicture: "url7", points: 75 },
-    { username: "user8", profilePicture: "url8", points: 40 },
-    { username: "user9", profilePicture: "url9", points: 85 },
-    { username: "user10", profilePicture: "url10", points: 120 },
-    { username: "user11", profilePicture: "url11", points: 55 },
-    { username: "user12", profilePicture: "url12", points: 95 },
-    { username: "user13", profilePicture: "url13", points: 70 },
-    { username: "user14", profilePicture: "url14", points: 50 },
-    { username: "user15", profilePicture: "url15", points: 105 },
-    { username: "user16", profilePicture: "url16", points: 65 },
-    { username: "user17", profilePicture: "url17", points: 110 },
-    { username: "user18", profilePicture: "url18", points: 75 },
-    { username: "user19", profilePicture: "url19", points: 40 },
-    { username: "user20", profilePicture: "url20", points: 85 },
-    { username: "user21", profilePicture: "url21", points: 120 },
-    { username: "user22", profilePicture: "url22", points: 55 },
-    { username: "user23", profilePicture: "url23", points: 95 },
-    { username: "user24", profilePicture: "url24", points: 70 },
-    { username: "user25", profilePicture: "url25", points: 50 },
-    { username: "user26", profilePicture: "url26", points: 105 },
-    { username: "user27", profilePicture: "url27", points: 65 },
-    { username: "user28", profilePicture: "url28", points: 110 },
-    { username: "user29", profilePicture: "url29", points: 75 },
-    { username: "user30", profilePicture: "url30", points: 40 },
-    { username: "user31", profilePicture: "url31", points: 85 },
-    { username: "user32", profilePicture: "url32", points: 120 },
-    { username: "user33", profilePicture: "url33", points: 55 },
-    { username: "user34", profilePicture: "url34", points: 95 },
-    { username: "user35", profilePicture: "url35", points: 70 },
-    { username: "user36", profilePicture: "url36", points: 50 },
-    { username: "user37", profilePicture: "url37", points: 105 },
-    { username: "user38", profilePicture: "url38", points: 65 },
-    { username: "user39", profilePicture: "url39", points: 110 },
-    { username: "user40", profilePicture: "url40", points: 75 },
-    { username: "user41", profilePicture: "url41", points: 40 },
-    { username: "user42", profilePicture: "url42", points: 85 },
-    { username: "user43", profilePicture: "url43", points: 120 },
-    { username: "user44", profilePicture: "url44", points: 55 },
-    { username: "user45", profilePicture: "url45", points: 95 },
-    { username: "user46", profilePicture: "url46", points: 70 },
-    { username: "user47", profilePicture: "url47", points: 50 },
-    { username: "user48", profilePicture: "url48", points: 105 },
-    { username: "user49", profilePicture: "url49", points: 65 },
-    { username: "user50", profilePicture: "url50", points: 110 },
-  ];
-
+  
   return (
     <>
-      <div className="w-2/3 h-96 bg-green-600">
-        {/* <LeftCanvas
+      <div className="flex w-full h-full">
+        <LeftCanvas
           timetosort={timetosort}
           sorting={Sorting}
           time={time}
@@ -188,8 +140,8 @@ export default function Home() {
           randomButton={randomButton}
           number_of_bars={number_of_bars}
           setNumber_of_bars={setNumber_of_bars}
-        /> */}
-        <LeaderBoard winner={usersData} />
+        />
+        <RightCanvas bars={sampleArray} />
       </div>
     </>
   );
